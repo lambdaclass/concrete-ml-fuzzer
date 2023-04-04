4ac9698c73456b84f33b8ae978587cda769c09bf
juanma <juan.viel@lambdaclass.com>
2023-04-04 12:34:14 -0300

    Use atheris fuzzer

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # concrete-ml-fuzzer
 A differential fuzzer to compare [concrete-ml](https://docs.zama.ai/concrete-ml) against [scikit-learn](https://scikit-learn.org)
+
+## Dependencies
+- scikit-learn
+- concrete-ml
+- atheris
+
+These can be installed with:
+```sh
+pip install concrete-ml atheris
+```
+Installing concrete-ml with pip will install `scikit-learn` 
--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,3 +1,6 @@
+import sys
+import random
+import atheris
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression as lr_sklearn
@@ -8,7 +11,7 @@ x, y = make_classification(n_samples=100, class_sep=2, n_features=30, random_sta
 
 # Split the data-set into a train and test set,
 # each set is split into input and result.
-input_train, input_test, result_train, _ = train_test_split(
+input_train, _, result_train, _ = train_test_split(
     x, y, test_size=0.2, random_state=42
 )
 
@@ -20,15 +23,21 @@ concrete_model.fit(input_train, result_train)
 # Compile FHE
 concrete_model.compile(input_train)
 
-# Run the inference on encrypted inputs
-fhe_pred = concrete_model.predict(input_test, execute_in_fhe=True)
-
 # Start the sklearn logistic regression model
 scikit_model = lr_sklearn()
 # Train
 scikit_model.fit(input_train, result_train)
 
-# Get scikit prediction
-prediction = scikit_model.predict(input_test)
+#test = [[random.random() for _ in range(30)] for _ in range(2)]
 
-assert((fhe_pred == prediction).all())
+def compare_models(input_bytes):
+    fdp = atheris.FuzzedDataProvider(input_bytes)
+    data = [fdp.ConsumeFloatListInRange(30, 0.0, 1.0) for _ in range(2)]
+    # Run the inference on encrypted inputs
+    fhe_pred = concrete_model.predict(data, execute_in_fhe=True)
+    # Get scikit prediction
+    prediction = scikit_model.predict(data)
+    assert((fhe_pred == prediction).all())
+
+atheris.Setup(sys.argv, compare_models)
+atheris.Fuzz()
