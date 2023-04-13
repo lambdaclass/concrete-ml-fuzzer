eb0c90d954218cc1615ad468de8de789baccdea7
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-13 15:45:41 -0300

    Data cleanup

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -8,13 +8,12 @@ input_train, result_train = make_regression(n_samples=100, n_features=5, random_
 concrete_model, scikit_model = LinearRegression(n_bits=12).fit_benchmark(input_train, result_train)
 concrete_model.compile(input_train)
 
-data = [[uniform(-1.1, 10) for _ in range(5)] for _ in range(100)]
 prediction = scikit_model.predict(data)
 fhe_pred = concrete_model.predict(data, execute_in_fhe=True).flatten()
 
 def compare_models(input_bytes):
     fdp = atheris.FuzzedDataProvider(input_bytes)
-    data = [fdp.ConsumeFloatListInRange(5, -1.1, 4.0) for _ in range(100)]
+    data = [fdp.ConsumeFloatListInRange(5, -1.5, 4.0) for _ in range(100)]
     # Run the inference, encryption and decryption is done in the background
     fhe_pred = concrete_model.predict(data, execute_in_fhe=True).flatten()
     # Get scikit prediction
