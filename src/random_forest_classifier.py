cf4a164cfbf3e766c9f4f389db9abd81c808faa9
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-13 16:11:46 -0300

    Make changes random forest

--- src/random_forest_classifier.py
+++ src/random_forest_classifier.py
@@ -1,29 +1,28 @@
 import sys 
+import numpy
 import atheris
 from sklearn.datasets import make_blobs
-from sklearn.ensemble import RandomForestClassifier as SklearnRandomForest
-from concrete.ml.sklearn.rf import RandomForestClassifier as ConcreteRandomForest
+from concrete.ml.sklearn.rf import RandomForestClassifier
 
 train_x, train_y = make_blobs(n_samples=1000, n_features=10, centers=100,
     random_state=42)
-
-sklearn_model = SklearnRandomForest(n_estimators=10, max_depth=None,
-    min_samples_split=2, random_state=42).fit(train_x, train_y)
-
-concrete_model = ConcreteRandomForest(n_estimators=10, max_depth=None,
-    min_samples_split=2, random_state=42).fit(train_x, train_y)
+concrete_model, sklearn_model = RandomForestClassifier(n_bits=12, n_estimators=10, max_depth=None,
+    min_samples_split=2, random_state=42).fit_benchmark(train_x, train_y)
 concrete_model.compile(train_x)
 
 def compare_models(input_bytes):
     fdp = atheris.FuzzedDataProvider(input_bytes)
-    data = [fdp.ConsumeFloatListInRange(10, -13.0, 13.0) for _ in range(100)]
+    data = [fdp.ConsumeFloatListInRange(1000, -13.0, 13.0) for _ in range(100)]
     # Run the inference, encryption and decryption is done in the background
     fhe_pred = concrete_model.predict(data, execute_in_fhe=True)
     # Get scikit prediction
-    prediction = scikit_model.predict(data)
+    sk_pred = scikit_model.predict(data)
 
-    # Compare both outputs
-    assert((fhe_pred == prediction).all())
+    # Get the mean absolute percentage error(mape) to make sure the accuaracy is around 99%
+    mape = numpy.mean(numpy.abs((sk_pred - fhe_pred) / sk_pred))
+
+    # Compare outputs
+    assert(mape < 1.0)
 
 atheris.Setup(sys.argv, compare_models)
 atheris.Fuzz()
--- src/random_forest_regressor.py
+++ src/random_forest_regressor.py
@@ -1,17 +1,15 @@
 import sys 
+import numpy
 import atheris
 from sklearn.datasets import make_blobs
-from sklearn.ensemble import RandomForestRegressor as SklearnRandomForest
-from concrete.ml.sklearn.rf import RandomForestRegressor as ConcreteRandomForest
+from concrete.ml.sklearn.rf import RandomForestRegressor
 
 train_x, train_y = make_blobs(n_samples=1000, n_features=10, centers=100,
     random_state=42)
 
-sklearn_model = SklearnRandomForest(n_estimators=10, max_depth=None,
-    min_samples_split=2, random_state=42).fit(train_x, train_y)
+concrete_model, sklearn_model = RandomForestClassifier(n_bits=12, n_estimators=10, max_depth=None,
+    min_samples_split=2, random_state=42).fit_benchmark(train_x, train_y)
 
-concrete_model = ConcreteRandomForest(n_estimators=10, max_depth=None,
-    min_samples_split=2, random_state=42).fit(train_x, train_y)
 concrete_model.compile(train_x)
 
 def compare_models(input_bytes):
@@ -20,10 +18,13 @@ def compare_models(input_bytes):
     # Run the inference, encryption and decryption is done in the background
     fhe_pred = concrete_model.predict(data, execute_in_fhe=True)
     # Get scikit prediction
-    prediction = scikit_model.predict(data)
+    sk_pred = scikit_model.predict(data)
 
-    # Compare both outputs
-    assert((fhe_pred == prediction).all())
+    # Get the mean absolute percentage error(mape) to make sure the accuaracy is around 99%
+    mape = numpy.mean(numpy.abs((sk_pred - fhe_pred) / sk_pred))
+
+    # Compare outputs
+    assert(mape < 1.0)
 
 atheris.Setup(sys.argv, compare_models)
 atheris.Fuzz()
