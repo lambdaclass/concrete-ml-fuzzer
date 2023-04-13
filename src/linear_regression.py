1905046d375664aadc8539dcbd1b549dfe131a33
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-13 15:44:35 -0300

    Evaluate mean percentage error

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -1,27 +1,30 @@
 import sys
 import atheris
 from sklearn.datasets import make_regression
-from sklearn.linear_model import LinearRegression as SklearnLinearRegression
-from concrete.ml.sklearn import LinearRegression as ConcreteLinearRegression
-
+from concrete.ml.sklearn import LinearRegression
 # Regression train set
 input_train, result_train = make_regression(n_samples=100, n_features=5, random_state=42)
 
-concrete_model = ConcreteLinearRegression().fit(input_train, result_train)
+concrete_model, scikit_model = LinearRegression(n_bits=12).fit_benchmark(input_train, result_train)
 concrete_model.compile(input_train)
 
-scikit_model = SklearnLinearRegression().fit(input_train, result_train)
+data = [[uniform(-1.1, 10) for _ in range(5)] for _ in range(100)]
+prediction = scikit_model.predict(data)
+fhe_pred = concrete_model.predict(data, execute_in_fhe=True).flatten()
 
 def compare_models(input_bytes):
     fdp = atheris.FuzzedDataProvider(input_bytes)
-    data = [fdp.ConsumeFloatListInRange(5, -3.0, 3.0) for _ in range(100)]
+    data = [fdp.ConsumeFloatListInRange(5, -1.1, 4.0) for _ in range(100)]
     # Run the inference, encryption and decryption is done in the background
-    fhe_pred = concrete_model.predict(data, execute_in_fhe=True)
+    fhe_pred = concrete_model.predict(data, execute_in_fhe=True).flatten()
     # Get scikit prediction
     prediction = scikit_model.predict(data)
 
-    # Compare both outputs
-    assert((fhe_pred == prediction).all())
+    # Get the mean percentage error to make sure the accuaracy is around 99%
+    mean_per_err = 100 - abs(sum([ 1 - f / a  for a, f in zip(prediction, fhe_pred)]))
+
+    # Compare outputs
+    assert(mean_per_err > 99.0)
 
 atheris.Setup(sys.argv, compare_models)
 atheris.Fuzz()
