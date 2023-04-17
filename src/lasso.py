e11ae02f89be10617ca967ac1d51d3e094f8746c
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-17 10:24:37 -0300

    Add models: ElasticNet, Ridge, Lasso and LinearSVC

---
+++ src/lasso.py
@@ -0,0 +1,16 @@
+import sys
+import atheris
+from concrete.ml.sklearn import Lasso 
+from utils import initialize_models, mean_absolute_percentage_error, consume_bytes
+
+concrete_model, scikit_model, data_info = initialize_models(Lasso)
+
+def compare_models(input_bytes):
+    data = consume_bytes(input_bytes, data_info, margin=0.1)
+    fhe_pred = concrete_model.predict(data, execute_in_fhe=True).flatten()
+    prediction = scikit_model.predict(data)
+
+    assert(mean_absolute_percentage_error(prediction, fhe_pred) < 1)
+
+atheris.Setup(sys.argv, compare_models)
+atheris.Fuzz()
---
+++ src/linear_svc.py
@@ -0,0 +1,19 @@
+import sys
+import atheris
+from concrete.ml.sklearn import LinearSVC
+from utils import initialize_models, mean_absolute_percentage_error, consume_bytes
+
+concrete_model, scikit_model, data_info = initialize_models(LinearSVC)
+
+def compare_models(input_bytes):
+    data = consume_bytes(input_bytes, data_info)
+    # Run the inference, encryption and decryption is done in the background
+    fhe_pred = concrete_model.predict(data, execute_in_fhe=True).flatten()
+    # Get scikit prediction
+    prediction = scikit_model.predict(data)
+
+    # Get the mean percentage error to make sure the accuaracy is around 99%
+    assert(mean_absolute_percentage_error(prediction, fhe_pred) < 1)
+
+atheris.Setup(sys.argv, compare_models)
+atheris.Fuzz()
