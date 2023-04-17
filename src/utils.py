8ee904811c0573eaaf132dfaf47bb4e85b09d721
Juan-M-V <102986292+Juan-M-V@users.noreply.github.com>
2023-04-17 14:45:17 -0300

    Update src/utils.py
    Co-authored-by: Mario Rugiero <mario.rugiero@nextroll.com>

--- src/utils.py
+++ src/utils.py
@@ -10,8 +10,8 @@ def _classification_training() -> (np.array, np.array, dict):
     training_x, training_y = load_iris(return_X_y=True)
     data_info = {
         "dimensions": 4,
-        "min_feature": min([ min(minimum) for minimum in training_x]),
-        "max_feature": max([ max(maximum) for maximum in training_x]),
+        "min_feature": min(min(minimum) for minimum in training_x),
+        "max_feature": max(max(maximum) for maximum in training_x),
     }
 
     return (training_x, training_y, data_info)
