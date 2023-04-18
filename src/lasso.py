1b800e0f9e9bc1b254b04863e5ea2e9b3c48387c
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-18 12:27:34 -0300

    Capitalize FHE acronym

--- src/lasso.py
+++ src/lasso.py
@@ -7,10 +7,10 @@ concrete_model, scikit_model, data_info = initialize_models(Lasso, {"n_bits":12}
 
 def compare_models(input_bytes):
     data = consume_bytes(input_bytes, data_info, margin=0.1)
-    fhe_pred = concrete_model.predict(data, execute_in_fhe=True)
+    FHE_pred = concrete_model.predict(data, execute_in_FHE=True)
     prediction = scikit_model.predict(data)
 
-    assert(mean_absolute_percentage_error(prediction, fhe_pred) < 2)
+    assert(mean_absolute_percentage_error(prediction, FHE_pred) < 2)
 
 atheris.Setup(sys.argv, compare_models)
 atheris.Fuzz()
--- src/linear_svc.py
+++ src/linear_svc.py
@@ -8,12 +8,12 @@ concrete_model, scikit_model, data_info = initialize_models(LinearSVC, {"n_bits"
 def compare_models(input_bytes):
     data = consume_bytes(input_bytes, data_info)
     # Run the inference, encryption and decryption is done in the background
-    fhe_pred = concrete_model.predict(data, execute_in_fhe=True)
+    FHE_pred = concrete_model.predict(data, execute_in_FHE=True)
     # Get scikit prediction
     prediction = scikit_model.predict(data)
 
     # Get the mean percentage error to make sure the accuaracy is around 99%
-    assert(mean_absolute_percentage_error(prediction, fhe_pred) < 1)
+    assert(mean_absolute_percentage_error(prediction, FHE_pred) < 1)
 
 atheris.Setup(sys.argv, compare_models)
 atheris.Fuzz()
