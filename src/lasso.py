6dfffc1b1847746840ff4193b93dc0a69c733fcc
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-19 17:42:15 -0300

    Add commments

--- src/lasso.py
+++ src/lasso.py
@@ -6,8 +6,11 @@ from utils import initialize_models, mean_absolute_percentage_error, consume_byt
 concrete_model, scikit_model, data_info = initialize_models(Lasso)
 
 def compare_models(input_bytes):
+    # Run the inference, encryption and decryption is done in the background
     data = consume_bytes(input_bytes, data_info, margin=0.1)
+    # Get scikit prediction
     FHE_pred = concrete_model.predict(data, execute_in_fhe=True)
+    # Get the mean percentage error to make sure the accuaracy is around 98%
     prediction = scikit_model.predict(data)
 
     assert(mean_absolute_percentage_error(prediction, FHE_pred) < 2)
