9c4f158640dfccf7a1d0c634a6601f80aa13a591
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-19 17:36:32 -0300

    Use utils

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,41 +1,19 @@
 import sys
 import atheris
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression as SklearnLogisticRegression
-from concrete.ml.sklearn import LogisticRegression as ConcreteLogisticRegression
+from concrete.ml.sklearn import LogisticRegression 
+from utils import initialize_models, mean_absolute_percentage_error, consume_bytes
 
-# Dataset to train
-x, y = make_classification(n_samples=100, class_sep=2, n_features=5, random_state=42)
-
-# Split the data-set into a train and test set,
-# each set is split into input and result.
-input_train, _, result_train, _ = train_test_split(
-    x, y, test_size=0.2, random_state=42
-)
-
-# Start the concrete-ml logistic regression model, train (unencrypted data) and quantize the weights.
-concrete_model = ConcreteLogisticRegression(n_bits=12)
-concrete_model.fit(input_train, result_train)
-
-# Compile FHE
-concrete_model.compile(input_train)
-
-# Start the sklearn logistic regression model
-scikit_model = SklearnLogisticRegression()
-# Train
-scikit_model.fit(input_train, result_train)
+concrete_model, scikit_model, data_info = initialize_models(LogisticRegression)
 
 def compare_models(input_bytes):
-    fdp = atheris.FuzzedDataProvider(input_bytes)
-    data = [fdp.ConsumeFloatListInRange(5, -1.0, 1.0) for _ in range(100)]
+    data = consume_bytes(input_bytes, data_info, margin=0.1)
     # Run the inference, encryption and decryption is done in the background
-    fhe_pred = concrete_model.predict(data, execute_in_fhe=True)
+    FHE_pred = concrete_model.predict(data, execute_in_fhe=True)
     # Get scikit prediction
     prediction = scikit_model.predict(data)
 
-    # Compare both outputs
-    assert((fhe_pred == prediction).all())
+    # Get the mean percentage error to make sure the accuaracy is around 99%
+    assert(mean_absolute_percentage_error(prediction, FHE_pred) < 1)
 
 atheris.Setup(sys.argv, compare_models)
 atheris.Fuzz()
