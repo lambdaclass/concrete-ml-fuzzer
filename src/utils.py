f59249d8f5b9c30a39609746de947eb7b277257e
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-20 16:37:12 -0300

    Correct dividing by zero and range

--- src/utils.py
+++ src/utils.py
@@ -10,8 +10,8 @@ def _classification_training() -> (np.array, np.array, dict):
     training_x, training_y = load_iris(return_X_y=True)
     data_info = {
         "dimensions": 4,
-        "min_feature": min(min(minimum) for minimum in training_x),
-        "max_feature": max(max(maximum) for maximum in training_x),
+        "min_feature": np.amin(training_x, axis=0),
+        "max_feature": np.amax(training_x, axis=0)
     }
 
     return (training_x, training_y, data_info)
@@ -23,8 +23,8 @@ def _regression_training() -> (np.array, np.array, dict):
     training_x, training_y = make_regression(n_samples=100, n_features=5, random_state=42)
     data_info = {
         "dimensions": 5,
-        "min_feature": min(min(minimum) for minimum in training_x),
-        "max_feature": max(max(maximum) for maximum in training_x),
+        "min_feature": np.amin(training_x, axis=0),
+        "max_feature": np.amax(training_x, axis=0)
     }
 
     return (training_x, training_y, data_info)
@@ -38,28 +38,26 @@ def consume_bytes(input_bytes: bytes, data_info, n_samples=1000, margin=0.1) -> 
     margin: margin to expand the dataset range beyond the range of the training set. Recomended to use small values
     """
     fdp = atheris.FuzzedDataProvider(input_bytes)
-    data = [
-            fdp.ConsumeFloatListInRange(
-                data_info["dimensions"], 
-                data_info["min_feature"] - margin, 
-                data_info["max_feature"] + margin
-            ) for _ in range(n_samples)
-           ]
+    data= [
+            [
+                fdp.ConsumeFloatInRange(data_info["min_feature"][i], data_info["max_feature"][i]) for i in range(data_info["dimensions"])
+            ] for _ in range(n_samples)
+    ]
     return data
 
 def mean_absolute_percentage_error(y_sklearn, y_FHE) -> float:
     """
-    Calculate the mean absolute percentage error to determine how much the FHE model deviates from the sklearn model.
+    Calculate the mean absolute percentage error to determine how much the fhe model deviates from the sklearn model.
     Values fall in the (0, 100) range, lower values represent less deviation.
     """
-    
+
     # Compute accuracy for each possible representation
-    score = np.abs((y_sklearn - y_FHE) / y_sklearn)
+    score = np.abs((y_sklearn - y_FHE ) / y_sklearn)
+    score = score[np.isfinite(score)]
 
-    return np.mean(score)
+    return np.mean(score) if np.any(score) else 0
 
-
-def initialize_models(ModelClass, n_bits=12, *params):
+def initialize_models(ModelClass, params={"n_bits":12}):
     """
     Initialize concrete and sklearn models. Automatically load iris or diabetes dataset
     """
@@ -68,7 +66,7 @@ def initialize_models(ModelClass, n_bits=12, *params):
     
     # The n_bits parameter represents the number of bits used for the model quantization. 
     # Bigger numbers achieve a higher accuracy at the cost of speed. The maximum possible value (v1.0.0) is 12. 
-    model = ModelClass(n_bits=n_bits, *params)
+    model = ModelClass(**params)
     concrete_model, sklearn_model = model.fit_benchmark(X, y)
     concrete_model.compile(X)
     return concrete_model, sklearn_model, data_info
