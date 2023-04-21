7fba9de511cadaf2e78e7711fbd06039be29e5f6
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-21 11:54:51 -0300

    Update utils

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,6 @@ from sklearn.preprocessing import (
     StandardScaler,
 )
 
-
 def _classification_training() -> (np.array, np.array, dict):
     """
     Create a classification training set
@@ -21,8 +20,8 @@ def _classification_training() -> (np.array, np.array, dict):
     training_x, training_y = load_iris(return_X_y=True)
     data_info = {
         "dimensions": 4,
-        "min_feature": min(min(minimum) for minimum in training_x),
-        "max_feature": max(max(maximum) for maximum in training_x),
+        "min_feature": np.amin(training_x, axis=0),
+        "max_feature": np.amin(training_x, axis=0),
     }
 
     return (training_x, training_y + 1, data_info)
@@ -134,13 +133,11 @@ def consume_bytes(input_bytes: bytes, data_info, n_samples=1000, margin=0.1) -> 
     margin: margin to expand the dataset range beyond the range of the training set. Recomended to use small values
     """
     fdp = atheris.FuzzedDataProvider(input_bytes)
-    data = [
-            fdp.ConsumeFloatListInRange(
-                data_info["dimensions"], 
-                data_info["min_feature"] - margin, 
-                data_info["max_feature"] + margin
-            ) for _ in range(n_samples)
-           ]
+    data= [
+            [
+                fdp.ConsumeFloatInRange(data_info["min_feature"][i] - margin, data_info["max_feature"][i] + margin) for i in range(data_info["dimensions"])
+            ] for _ in range(n_samples)
+    ]
     return data
 
 def mean_absolute_percentage_error(y_sklearn, y_FHE) -> float:
