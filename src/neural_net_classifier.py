84d5b129ef9a9ea947d1a5a191cf6d76fe41bd9e
Juanma <juanma@Juanmas-MacBook-Air.local>
2023-04-21 11:43:46 -0300

    Compare models, use utils

--- src/neural_net_classifier.py
+++ src/neural_net_classifier.py
@@ -1,21 +1,17 @@
 import sys
 import atheris
-from random import uniform
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from concrete.ml.sklearn import NeuralNetClassifier as ConcreteNN
+from concrete.ml.sklearn import NeuralNetClassifier
+from utils import consume_bytes, initialize_models, mean_absolute_percentage_error
 
-train_input, train_result = make_classification(n_samples=100, class_sep=2, n_features=5, random_state=42)
+concrete_model, sklearn_model, data_info = initialize_models(NeuralNetClassifier, {})
 
-params = {
-    "module__input_dim": 1,
-    "module__activation_function": nn.Sigmoid,
-}
+def compare_models(input_bytes):
+    data = consume_bytes(input_bytes, data_info, n_samples=10)
 
-concrete_model = ConcreteNN(**params).fit(train_input, train_result)
+    FHE_pred = concrete_model.predict(data, execute_in_fhe=True)
+    prediction = sklearn_model.predict(data)
 
-concrete_model.compile()
+    assert(mean_absolute_percentage_error(prediction, FHE_pred) < 1)
 
-data = [ [ uniform(-5.0, 5.0) for _ in range(5)] for _ in range(20) ]
-
-pred = concrete_model.predict(data)
+atheris.Setup(sys.argv, compare_models)
+atheris.Fuzz()
